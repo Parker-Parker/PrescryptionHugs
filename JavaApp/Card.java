1ccdd1936400038cb255ea94e0b7f99c6ca6bb27
Parker-Parker <ParkerAGrant@yahoo.com>
2023-05-26 13:44:26 -0400

    made takeDamage() in Card made checkSigil() in Card made getBuff() in Card made getAttacks() in Card

--- JavaApp/Card.java
+++ JavaApp/Card.java
@@ -10,7 +10,9 @@ public class Card implements iCard {
     int baseHealth = 1;
     int attack = 0;
     int health = 1;
-    
+
+    int[][] attackLists= {{0}, {-1,1}, {-1,0,1}, {-2,-1,0,1,2} };
+
 
     public Card() {
         title = title + id;
@@ -32,27 +34,60 @@ public class Card implements iCard {
     public void onSummon(Field field) {
     }
 
-    public void doAttack() {
-    }
 
     public int[] getAttacks(int i) {
-        return null;
+        if(this.checkSigil(Sigils.BifurcatedStrike)){
+            return attackLists[1];
+        }  else
+        if(this.checkSigil(Sigils.TrifurcatedStrike)){
+            return attackLists[2];
+        }  else
+        if(this.checkSigil(Sigils.OmniStrike)){
+            return attackLists[3];
+        }
+        else {
+            return attackLists[0];
+        }
     }
 
     public int getBuff(int i) {
-        return 0;
+        switch(i){
+            case 0:
+            return this.checkSigil(Sigils.Stinky) ? -1 : 0;
+            case -1:
+            case 1:
+            return this.checkSigil(Sigils.Leader) ? 1 : 0;
+            default:
+            return 0;            
+        }
     }
 
-    public boolean checkSigil(Sigils waterborne) {
-        return false;
+    public boolean checkSigil(Sigils sigil) {
+        return sigils.get(sigil);
     }
 
     public int takeDamage(int damage, Card card) {
-        return 0;
+        int overflow = damage>health ? damage-health : 0;
+        if(damage>0){
+
+            this.health = this.health - damage;
+            if(checkSigil(Sigils.SharpQuills)){
+                if(!(card == null)){
+                    card.takeDamage(1, null);//realistically, if i wanted this to be robust, I would always pass the dmg src, and add a list of damage tags(poison, quill, etc). oh well
+                } 
+            }
+        }
+        if(card==null){
+            System.out.println(title + " took " + damage + " point of revenge damage");
+        } 
+        else {
+            System.out.println(card.title +  " dealt " + damage + " to " + title);
+        }
+        return overflow;
     }
 
     public int getBaseAttack() {
-        return 0;
+        return baseAttack;
     } 
     
 }
