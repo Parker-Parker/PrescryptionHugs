b728c47e499fe311d77966dcf6a9db765a16b6e2
Parker-Parker <pagrant@wpi.edu>
2023-06-14 07:33:39 -0400

    ugh idk man

--- JavaApp/Server copy.java
+++ JavaApp/Server copy.java
@@ -1,17 +1,56 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
-class TCPServer2 {
+class TCPServer extends Thread{
+
+    String clientSentence;
+    String capitalizedSentence;
+    ServerSocket welcomeSocket;// = new ServerSocket(5433);
+    Socket = connectionSocket;
+
     public static void main(String argv[]) throws Exception {
         String clientSentence;
         String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(5433);
+
+        System.out.println("Server Set Up: "    + "\nlocPort: "+ welcomeSocket.getLocalPort()
+                                                + "\ninet: "+ welcomeSocket.getInetAddress()
+                                                + "\nlocSoc: "+ welcomeSocket.getLocalSocketAddress()
+                                                + "\nchnl: "+ welcomeSocket.getChannel()
+                        );
+
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            // BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
+            
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
+
+            clientSentence = inFromClient.nextLine();
+            System.out.println("Received: " + clientSentence + " on " + connectionSocket.getPort() +" Inet:"+ connectionSocket.getInetAddress()
+                                                                                                    +" loc:"+ connectionSocket.getLocalAddress()
+                                                                                                    +" LocSoc:"+ connectionSocket.getLocalSocketAddress()
+                                                                                                    +" Rem Soc:"+ connectionSocket.getRemoteSocketAddress()
+                                                                                                    +" Inet:"+ connectionSocket.getChannel());
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
+    public void run(){
+        while (true) {
+            // Socket connectionSocket = welcomeSocket.accept();
+            // BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
+            
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.nextLine();
+            System.out.println("Received: " + clientSentence + " on " + connectionSocket.getPort() +" Inet:"+ connectionSocket.getInetAddress()
+                                                                                                    +" loc:"+ connectionSocket.getLocalAddress()
+                                                                                                    +" LocSoc:"+ connectionSocket.getLocalSocketAddress()
+                                                                                                    +" Rem Soc:"+ connectionSocket.getRemoteSocketAddress()
+                                                                                                    +" Inet:"+ connectionSocket.getChannel());
             capitalizedSentence = clientSentence.toUpperCase() + '\n';
             outToClient.writeBytes(capitalizedSentence);
         }
