80b571fe5859a1c142fa0c1818776b057b0d7c9a
Parker-Parker <ParkerAGrant@yahoo.com>
2023-06-23 12:26:57 -0400

    cleanup

--- JavaApp/NullCard.java
+++ JavaApp/NullCard.java
@@ -1,6 +1,6 @@
 
 public class NullCard extends Card{// this is a special "virtual" card, it is meant to represent striking an unoccupied card slot
- //TODO: implement this
+ 
     public boolean turn = true;//true if player is attacking
 
     public NullCard(Field field) {
@@ -9,22 +9,12 @@ public class NullCard extends Card{// this is a special "virtual" card, it is me
         this.title = "Null";
     }
     public int takeDamage(int damage, Card card) {
-        // int overflow = damage>health ? damage-health : 0;
-        // if(damage>0){
-
-        //     this.health = this.health - damage;
-        //     if(this.health <= 0){this.die();}
-        //     if(checkSigil(Sigils.SharpQuills)){
-        //         if(!(card == null)){
-        //             card.takeDamage(1, null);//realistically, if i wanted this to be robust, I would always pass the dmg src, and add a list of damage tags(poison, quill, etc). oh well
-        //         } 
-        //     }
-        // }
+        
         if(card==null){
             System.out.println(title + " took " + damage + " point of revenge damage //um... WHAT!?");
         } 
         else {
-            System.out.println(card.title +  " dealt " + damage + " to " + title +"  // TODO:fix this");
+            System.out.println(card.title +  " dealt " + damage + " to " + title);
             
             field.directDamage( this.turn? damage:-damage );//positive tips scale in player's favor
         }
