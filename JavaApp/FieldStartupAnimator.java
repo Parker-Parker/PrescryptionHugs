8323afc66bfe2602cb5354843ddcbae5cf1bc2f8
Parker-Parker <pagrant@wpi.edu>
2023-07-19 15:10:16 -0400

    Finished startup animator

--- JavaApp/FieldStartupAnimator.java
+++ JavaApp/FieldStartupAnimator.java
@@ -2,10 +2,12 @@
 public class FieldStartupAnimator {
 
     public static void animatePickup(Field finalField, ObserverOutputHandler observerOutputHandler) {
+        
+        int sent = 0;
         if(finalField!=null&&observerOutputHandler!=null){
             Field tempField = new Field();
 
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 tempField.getEnemyCardsBack()[col] = finalField.getEnemyCardsBack()[col] ;
                 tempField.getEnemyCards()[col] = finalField.getEnemyCards()[col] ;
                 tempField.getPlayerCards()[col] = finalField.getPlayerCards()[col] ;
@@ -13,46 +15,53 @@ public class FieldStartupAnimator {
             }
 
 
-
-
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 observerOutputHandler.publishAnim(tempField, 0, col, Animations.YeetOut);
                 tempField.getEnemyCardsBack()[col] = null;
+                sent++;
             }
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 observerOutputHandler.publishAnim(tempField, 1, col, Animations.YeetOut);
                 tempField.getEnemyCards()[col] = null ;
+                sent++;
             }
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 observerOutputHandler.publishAnim(tempField, 2, col, Animations.YeetOut);
                 tempField.getPlayerCards()[col] = null ;
+                sent++;
             }
 
 
 
         }
+        System.out.println("Sent "+sent +" field"+(finalField!=null)+" obsout:"+(observerOutputHandler!=null));
 
     }
 
     public static void animatePutdown(Field finalField, ObserverOutputHandler observerOutputHandler) {
+        int sent = 0;
         if(finalField!=null&&observerOutputHandler!=null){
             Field tempField = new Field();
             tempField.clear();
             
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 tempField.getPlayerCards()[col] = finalField.getPlayerCards()[col] ;
                 observerOutputHandler.publishAnim(tempField, 2, col, Animations.YeetIn);
+                sent++;
             }
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 tempField.getEnemyCards()[col] = finalField.getEnemyCards()[col] ;
                 observerOutputHandler.publishAnim(tempField, 1, col, Animations.YeetIn);
+                sent++;
             }
-            for(int col = 0;col >4;col++){
+            for(int col = 0;col <4;col++){
                 tempField.getEnemyCardsBack()[col] = finalField.getEnemyCardsBack()[col] ;
                 observerOutputHandler.publishAnim(tempField, 0, col, Animations.YeetIn);
+                sent++;
             }
             
         }
+        System.out.println("Sent "+sent +" field"+(finalField!=null)+" obsout:"+(observerOutputHandler!=null));
     }
 
 }
