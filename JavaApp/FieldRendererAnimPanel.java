4fd237dae647302f74ca43a866259a10d6a2777c
Parker-Parker <pagrant@wpi.edu>
2023-08-24 22:27:51 -0400

    added arrows

--- JavaApp/FieldRendererAnimPanel.java
+++ JavaApp/FieldRendererAnimPanel.java
@@ -67,6 +67,7 @@ public class FieldRendererAnimPanel extends JPanel implements ActionListener {
     BufferedImage cardWhite = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
     BufferedImage slotBase = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
     BufferedImage slotBaseU = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage slotBaseA = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
     BufferedImage slotBaseBlank = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
     BufferedImage slotBaseDeck = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
     BufferedImage slotBaseDiscard = new BufferedImage(CARD_WIDTH, CARD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
@@ -294,6 +295,11 @@ public class FieldRendererAnimPanel extends JPanel implements ActionListener {
         }
         try {
             slotBaseU = ImageIO.read(new File("JavaApp/resources/Slot/card_slot_flipped.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            slotBaseA = ImageIO.read(new File("JavaApp/resources/Slot/Arrow_slot.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -547,7 +553,7 @@ public class FieldRendererAnimPanel extends JPanel implements ActionListener {
 
         for (int i = 0; i < 4; i++) {
             if ((canvas != null) && (field != null) && (field.getEnemyCardsBack() != null) && (field.getEnemyCardsBack().length == 4)) {
-                renderAtPos(canvas, renderCardSlotU(renderCard(field.getEnemyCardsBack()[i]), getAnimation(i, 0)), i, 0);
+                renderAtPos(canvas, renderCardSlotA(renderCard(field.getEnemyCardsBack()[i]), getAnimation(i, 0)), i, 0);
             }
         }
 
@@ -842,6 +848,48 @@ public class FieldRendererAnimPanel extends JPanel implements ActionListener {
         // if(DRAW_SLOTS){
         if((HAND_CARDS_DRAW_MODE/4)%2==0&&firstMessageReceived){
             slotGraphics2d.drawImage(slotBaseU, null, (slot.getWidth()-slotBase.getWidth())/2, (slot.getHeight()-slotBase.getHeight())/2);
+
+        } else {
+            slotGraphics2d.drawImage(slotBaseBlank, null, (slot.getWidth() - slotBaseBlank.getWidth()) / 2,
+                    (slot.getHeight() - slotBaseBlank.getHeight()) / 2);
+
+        }
+
+        cardAnimatedGraphics2d.drawImage(card, null, (slot.getWidth() - card.getWidth()) / 2,
+                (slot.getHeight() - card.getHeight()) / 2);
+        // if(animationTransform!=null){
+        // // System.out.println("Did a tf: "+animationTransform.toString());
+        // BufferedImage cardAnimatedTransformed = new BufferedImage(CARD_WIDTH*4,
+        // CARD_HEIGHT*4, BufferedImage.TYPE_INT_ARGB);
+        // AffineTransformOp affineTransformOp = new
+        // AffineTransformOp(animationTransform, AffineTransformOp.TYPE_BILINEAR);
+        // affineTransformOp.filter(cardAnimated, cardAnimatedTransformed );
+        // cardAnimated = cardAnimatedTransformed;
+        // // cardAnimatedGraphics2d.transform(animationTransform);
+        // }
+        // slotGraphics2d.drawImage(cardAnimated, null, 0,0);
+
+        if (animationTransform == null) {// I think this is supposed to be faster...
+            slotGraphics2d.drawImage(cardAnimated, null, 0, 0);
+
+        } else {
+            slotGraphics2d.drawImage(cardAnimated,
+                    new AffineTransformOp(animationTransform, AffineTransformOp.TYPE_BILINEAR), 0, 0);
+        }
+
+        return slot;// returns a transparent image with a slot at the exaft center and the
+                    // corresponding card placed relative to the slot
+
+    }
+
+    private BufferedImage renderCardSlotA(BufferedImage card, AffineTransform animationTransform){
+        BufferedImage slot = new BufferedImage(CARD_WIDTH*4, CARD_HEIGHT*4, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage cardAnimated = new BufferedImage(CARD_WIDTH*4, CARD_HEIGHT*4, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D slotGraphics2d = (Graphics2D)slot.getGraphics();
+        Graphics2D cardAnimatedGraphics2d = (Graphics2D)cardAnimated.getGraphics();
+        // if(DRAW_SLOTS){
+        if((HAND_CARDS_DRAW_MODE/4)%2==0&&firstMessageReceived){
+            slotGraphics2d.drawImage(slotBaseA, null, (slot.getWidth()-slotBaseA.getWidth())/2, (slot.getHeight()-slotBaseA.getHeight())/2);
 
         } else {
             slotGraphics2d.drawImage(slotBaseBlank, null, (slot.getWidth() - slotBaseBlank.getWidth()) / 2,
